2bdaf729d7a29151d526423016932bd202422a92
jeremy <jeremyin760@outlook.com>
2026-07-19 18:54:28 -0400

    fixed issues in the routes.py

--- service/routes.py
+++ service/routes.py
@@ -63,9 +63,10 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-    """ 
+    """
     List all Accounts
     This endpoint will list all Accounts
     """
@@ -83,6 +84,8 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -103,6 +106,8 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -126,9 +131,11 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-@app.route("/accounts/<int:account_id>", methods = ["DELETE"])
+
+
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-    """ 
+    """
     Delete an Account
     This endpoint will delete an Account based on the account_id that is requested
     """
@@ -137,7 +144,7 @@ def delete_accounts(account_id):
 
     account = Account.find(account_id)
     if account:
-       account.delete()
+        account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
 
